fe37c1f05b92c305938a7f01733005839b8358b9
zack dove <zdove@live.co.uk>
2021-01-08 17:56:43 +0000

    More initialisation steps

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
 var ctx;
+var width = window.innerWidth,
+  height = window.innerHeight;
+
 
 
 // Helper Functions
 
+window.onresize = function() {
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
+}
+
 // Returns true if device has pointer/mouse (i.e. no touch)
 function hasPointer(){
 	var query = window.matchMedia("(hover: hover) and (pointer: fine)");
-	// if (query.matches){
-	// 	return true;
-	// } else {
-	// 	return false;
-	// }
 	return query.matches;
 }
 
 // Initialisation
 
-
-
+function initCanv() {
+    canv = document.getElementById("canvas");
+    ctx  = canv.getContext("2d");
+    canv.width  = width;
+    canv.height = height;
+}
 
 function init() {
     // isMobile = checkMobile();
     if (hasPointer()) {
-        // updateMinDist();
-        // initCanv();
-        // initSplashContent();
-        // initParts();
-        
+        initCanvas();
     }
     else {
-		// No pointer, so just show normal
-        // setTimeout(() => {
-        //     document.body.classList.remove("loading");
-        // }, 500);
-        // initFocusPoint();
-        // initMainContent();
+		// No pointer, so just show normal, remove loading, make easy for Saul/
+		// Other team
     }
 }
 
